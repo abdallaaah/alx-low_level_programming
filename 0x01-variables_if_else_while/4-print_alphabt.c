8412c28f5e8c6c0010179778861529c8f7b4ddeb
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-17 19:16:09 -0700

    r

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - the main
+ * Return: always(Sucess)
+ */
+int main(void)
+
+{
+
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+	if ( i != 'q' || i != 'e')
+	{
+	putchar (i);
+	}
+}
+putchar ('\n');
+return (0);
+
+}
