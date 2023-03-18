140eac4e2b53ff29e4b477f78262c39067d69f3f
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-17 19:38:08 -0700

    r

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the main
+ * Return: always(Sucess)
+ */
+int main(void)
+
+{
+
+char i;
+
+for (i = 0; i < 10; i++)
+{
+putchar(48+i);
+}
+putchar ('\n');
+return (0);
+
+}
