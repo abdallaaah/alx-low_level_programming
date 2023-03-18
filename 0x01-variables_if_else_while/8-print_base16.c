5edbe8534b0c80578cca8c7472cce934a370daf2
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-17 19:43:53 -0700

    r

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - the main
+ * Return: always(Sucess)
+ */
+int main(void)
+
+{
+
+int i;
+char a;
+
+for (i = 0; i < 10; i++)
+{
+putchar(48+i);
+}
+for (a = 'a'; a < 'g'; a++)
+{
+putchar(a);
+}
+putchar ('\n');
+return (0);
+
+}
