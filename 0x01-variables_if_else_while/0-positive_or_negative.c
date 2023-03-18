1bbfad035ff3b24121e6de5497d9e59f2e488889
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-17 17:58:36 -0700

    tt

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /**
  *  main - This the description of the main
- *  Retsrn: alwayes (Success)
+ *  Return: alwayes (Success)
  */
 int main(void)
 {
@@ -14,15 +14,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-	printf("%d is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-	printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	else if (n < 0)
 	{
-	printf("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
