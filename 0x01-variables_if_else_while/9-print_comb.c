efb85b793420c72260e3c606e2ed91899a84af93
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-17 19:48:37 -0700

    r

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - the main
+ * Return: always(Sucess)
+ */
+int main(void)
+
+{
+
+int i;
+
+for (i = 0; i < 10; i++)
+{
+putchar(48 + i);
+puthchar(44)
+putchar(32)
+}
+putchar ('\n');
+return (0);
+
+}
