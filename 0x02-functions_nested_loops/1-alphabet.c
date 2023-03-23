739af72bbc7404bbea1573fbeeb469d996a32397
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-23 16:15:56 -0700

    ok

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,12 @@
  *
  * Return: Always 0.
  */
-void print_alphabet()
+void print_alphabet(void)
 {
-    char i;
-    for(i='a';i<='z';i++)
-        {
-          _putchar(i);
-          
-
+char i;
+for(i = 'a'; i <= 'z'; i++)
+{
+       	_putchar(i);
 }
-  _putchar('\n');
+_putchar('\n');
 }
