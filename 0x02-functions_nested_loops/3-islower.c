722a459c30bcdb514213fe2caebca07f0e4d2dd7
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-24 13:09:23 -0700

    donz

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,16 +3,16 @@
 /**
 * _islower : check
 *@c: is argumaent
-* Return: return in lower 
+*Return: return in lower 
 */
 int _islower(int c)
 {
-if(islower(c))
+if (islower(c))
 {
-return(1);
+return (1);
 }
 else
 {
-return(0);
+return (0);
 }
 }
