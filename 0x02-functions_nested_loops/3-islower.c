c9347977f3b6149153c4e6d54e204485117f25c8
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-23 18:58:10 -0700

    r

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,16 +2,18 @@
 #include <ctype.h>
 /**
  * _islower : check
- * Return: return0 in lower 
+ * c: is argumaent
+ * Return: return in lower 
  */
 int _islower(int c)
 {
 if (islower(c))
 {
-return 1;
+return (1);
 }
 else
 {
-return 0;
+return (0);
+
 }
 }
