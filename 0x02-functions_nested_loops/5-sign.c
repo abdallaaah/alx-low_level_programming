bfb0b970ca1b0ea4736934e501d974ab2edeac78
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-24 14:01:33 -0700

    r

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,13 +16,13 @@ if (n > 0)
 }
 if (n == 0)
 {
-_putchar('0');
-return (0);
+	_putchar('0');
+	return (0);
 }
 if (n < 0)
 {
-_putchar('-');
-return (-1);
+	_putchar('-');
+	return (-1);
 }
 return (0); 
 }
