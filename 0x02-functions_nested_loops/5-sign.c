ad6c9ac27263bf2d9353ce2dcd92b16776a6716b
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-24 13:58:01 -0700

    r

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,11 +9,10 @@
 
 int print_sign(int n)
 {
-
 if (n > 0)
 {
-_putchar('+');
-return (1);
+	_putchar('+');
+	return (1);
 }
 if (n == 0)
 {
@@ -26,5 +25,4 @@ _putchar('-');
 return (-1);
 }
 return (0); 
-
 }
