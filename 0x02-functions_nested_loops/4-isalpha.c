9ebd902d5ca162a09a2ab44072699bcb36672ade
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-23 21:20:31 -0700

    done

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+
+/**
+ * _isalpha -> check the letter
+ *@c: declare
+ * Return: return 1 in case of letters
+ */
+
+int _isalpha(int c)
+{
+if isalpha(c)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
