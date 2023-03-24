f71932eb776c1ebbc0691f061c013f2d5c73503e
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-24 12:52:53 -0700

    done

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+/*
+ * print_last_digit -> get the last digit 
+ * @num: number will play with
+ * @x: last digit
+ * Return: return the last digit 
+ */
+int print_last_digit(int num)
+{
+int x;
+x = num % 10;
+return (x);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,2 @@
 int _putchar(char c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
+int _islower(int c);
