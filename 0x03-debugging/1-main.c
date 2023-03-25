f76f51719654c6cea8837dfafa6b48f402ed2d01
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-25 10:49:41 -0700

    r

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-        int i;
+int i;
 
-        printf("Infinite loop incoming :(\n");
-	i = 0;
+printf("Infinite loop incoming :(\n");
+i = 0;
       	
 	/**
        	 * while (i < 10)
@@ -19,7 +19,7 @@ int main(void)
          * }
          */
 
-        printf("Infinite loop avoided! \\o/\n");
+printf("Infinite loop avoided! \\o/\n");
 
-        return (0);
+return (0);
 }
