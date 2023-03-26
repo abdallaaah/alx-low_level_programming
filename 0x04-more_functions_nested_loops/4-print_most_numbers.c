bfa0ca13e9963580dcbf1552a2f287ed1feb779e
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-25 20:26:54 -0700

    donz

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * print_most_numbers - print numbers
+ * Return: return from 1 to 9
+ */
+void print_most_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+if (!(i == 2 || i == 4))
+{
+_putchar(48 + i);
+}
+}
+_putchar('\n');
+}
