9f9eaf1a1504e4c41573952094200bee3cb939a7
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-25 20:14:09 -0700

    done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * print_numbers - print numbers
+ * Return: return from 1 to 9
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+_putchar(48 + i);
+}
+}
