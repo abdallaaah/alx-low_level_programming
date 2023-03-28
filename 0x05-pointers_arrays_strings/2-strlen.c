315cbc62d09a4f61d3573063ae91d8d9d67bcb98
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-27 18:39:40 -0700

    done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - calculate the string lenght
+ * @s: string
+ * Return: the string length
+ */
+int _strlen(char *s)
+{
+return (strlen(s));
+}
