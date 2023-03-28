5b6db4199233d10fcc45b01369d02628dec04935
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-27 19:05:42 -0700

    de

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - print the string in rev
+ * @s: is the sting
+ */
+void print_rev(char *s)
+{
+int i;
+int length;
+length = strlen(s);
+for (i= length - 1; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
