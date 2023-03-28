744d49c23b5f753b1714094268468d57e6b08f2e
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-27 19:58:06 -0700

    dine

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,12 +10,9 @@ void puts2(char *str)
 int length;
 int i;
 length = strlen(str);
-for ( i=0; i < length - 1; i++)
-{
-if ( i % 2 == 0)
+for (i=0; i < length - 1; i+=2)
 {
 _putchar(str[i]);
-}
 }
 _putchar('\n');
 }
