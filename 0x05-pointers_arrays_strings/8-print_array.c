dae9d740e4dc01679bf5fd1953cbec769e5a58d7
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-28 07:31:55 -0700

    sg

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,10 +11,14 @@ void print_array(int *a, int n)
 int i = 0;
 for (i = 0; i < n; i++)
 {
+if (n == 0)
+{
+printf("\n");
+break;
+}
 if (i + 1 == n && n > 0)
 {
 printf("%d\n", a[i]);
-printf("\n");
 }
 else
 {
