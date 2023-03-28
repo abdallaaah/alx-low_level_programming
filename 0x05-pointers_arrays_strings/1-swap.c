179d09770b34ed377c0686302b9a3368d5bf6175
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-27 18:29:36 -0700

    donz

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,12 @@
 #include "main.h"
+/**
+ * swap_int - swap two integerans
+ * @a: first pointer
+ * @b: second one
+ */
 void swap_int(int *a, int *b)
 {
-int z=0;
+int z = 0;
 z = *a;
 *a = *b;
 *b = z;
