f9b89563c0fe4fec27b706894f274b8b32257357
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-28 08:36:59 -0700

    dine

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,11 @@
 #include "main.h"
 #include <string.h>
+/**
+ * *_strcpy - string copiy
+ * @dest: destiantion
+ * @src: source
+ * Return: dest
+ */
 char *_strcpy(char *dest, char *src)
 {
 strcpy(dest, src);
