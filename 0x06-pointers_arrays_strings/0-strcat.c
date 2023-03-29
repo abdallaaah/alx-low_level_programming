37857c6fdb5de359b290f3c683fda15ca154bf6a
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-29 14:23:09 -0700

    done

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - concatinate two string
+ * @dest: destinartion
+ * @src: course
+ * Return: the dest
+ */
+char *_strcat(char *dest, char *src)
+{
+strcat(dest, src);
+return (dest);
+}
