19fdfe4e15450f89401b09e2043acf863dc05f9d
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-29 14:46:34 -0700

    done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,11 @@
 #include "main.h"
 #include <string.h>
+/*
+ * strcmp - compare two string
+ * @s1: sds
+ * @s2: sfgd
+ * Return: result in int
+ */
 int _strcmp(char *s1, char *s2)
 {
 return (strcmp(s1, s2));
