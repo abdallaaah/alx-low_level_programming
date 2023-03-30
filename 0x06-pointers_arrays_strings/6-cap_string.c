51c86120a6170ed1dffa993001f0e33707b5cff2
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-29 18:20:44 -0700

    done

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+char *cap_string(char *str) {
+    int i = 0;
+
+    while (str[i] != '\0') {
+      
+        if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' 
+            || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' 
+            || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' 
+            || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' 
+            || str[i - 1] == '}') {
+            str[i] = toupper(str[i]);
+        }
+        i++;
+    }
+
+    return str;
+}
