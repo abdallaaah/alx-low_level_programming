70e80b7e70a030440820d5d3b2f50b358594f355
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-31 20:34:49 -0700

    try

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,31 +1,35 @@
 #include "main.h"
+#include <stdio.h>
 #include <string.h>
-/**
- * leet - encode function
- * @s: string
- * Return: return new string
- */
 char *leet(char *s)
 {
-    char *result = s;
-    char *leet_map[256] = {0};
-    leet_map['a'] = leet_map['A'] = "4";
-    leet_map['e'] = leet_map['E'] = "3";
-    leet_map['o'] = leet_map['O'] = "0";
-    leet_map['t'] = leet_map['T'] = "7";
-    leet_map['l'] = leet_map['L'] = "1";
-    
-    for (; *s; ++s) {
-        char *leet_char = leet_map[*s];
-        if (leet_char != NULL) {
-            for (; *leet_char; ++leet_char, ++result) {
-                *result = *leet_char;
-            }
-        } else {
-            *(result++) = *s;
-        }
-    }
-    *result = '\0';
-    return result;
+int i;
+int len;
+char arr[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+char c;
+arr[0] = '4';
+arr[4] = '3';
+arr[14] = '0';
+arr[19] = '7';
+arr[11] = '1';
+len = strlen(s);
+for(i = 0; i < len; i++)
+{
+c = s[i];
+if (c >= 'a' && c <= 'z')
+{
+s[i] = arr[c - 'a'];
+/*printf("the current letter is : %c\n",s[i]);*/
 }
-
+if (c >= 'A' && c <= 'Z')
+{
+s[i] = arr[c - 'A'];
+/*printf("the current letter is : %c\n",s[i]);*/
+}
+}
+for (i=0; i< len; i++)
+{
+printf("%c",s[i]);
+}
+return NULL;
+}
