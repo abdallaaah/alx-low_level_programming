822062d6bf1f8bedb991f680f2cbf8cae22c1bf7
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-31 20:31:07 -0700

    play

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number - print numbers
+ * @n: this the number
+ */
+void print_number(int n)
+{
+int x;
+_putchar(n+48);
+}
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,29 +7,25 @@
  */
 char *leet(char *s)
 {
-char *result;
-char *leet_map[256] = {0};
-result = s;
-leet_map['a'] = leet_map['A'] = "4";
-leet_map['e'] = leet_map['E'] = "3";
-leet_map['o'] = leet_map['O'] = "0";
-leet_map['t'] = leet_map['T'] = "7";
-leet_map['l'] = leet_map['L'] = "1";
-for ( ; *s; ++s)
-{
-char *leet_char = leet_map[*s];
-if (leet_char != NULL)
-{
-for ( ; *leet_char; ++leet_char, ++result)
-{
-*result = *leet_char;
+    char *result = s;
+    char *leet_map[256] = {0};
+    leet_map['a'] = leet_map['A'] = "4";
+    leet_map['e'] = leet_map['E'] = "3";
+    leet_map['o'] = leet_map['O'] = "0";
+    leet_map['t'] = leet_map['T'] = "7";
+    leet_map['l'] = leet_map['L'] = "1";
+    
+    for (; *s; ++s) {
+        char *leet_char = leet_map[*s];
+        if (leet_char != NULL) {
+            for (; *leet_char; ++leet_char, ++result) {
+                *result = *leet_char;
+            }
+        } else {
+            *(result++) = *s;
+        }
+    }
+    *result = '\0';
+    return result;
 }
-}
-else
-{
-*result++ = *s;
-}
-}
-*result = '\0';
-return (result);
-}
+
