f37867429cd5e1fa29f5d8b74541e30b2ded149c
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-03-31 20:52:50 -0700

    done

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,11 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
+/**
+ * leet - change charcter to numbers
+ * @s: is the string
+ * Return: return the string
+ */
 char *leet(char *s)
 {
 int i;
 int len;
-char arr[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+char arr[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 char c;
 arr[0] = '4';
 arr[4] = '3';
@@ -13,7 +19,7 @@ arr[14] = '0';
 arr[19] = '7';
 arr[11] = '1';
 len = strlen(s);
-for(i = 0; i < len; i++)
+for (i = 0; i < len; i++)
 {
 c = s[i];
 if (c >= 'a' && c <= 'z')
@@ -27,9 +33,5 @@ s[i] = arr[c - 'A'];
 /*printf("the current letter is : %c\n",s[i]);*/
 }
 }
-for (i=0; i< len; i++)
-{
-printf("%c",s[i]);
-}
-return NULL;
+return (s);
 }
