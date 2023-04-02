318414e76489f24740cab91f9399726ab13d4eb2
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-04-01 21:36:52 -0700

    done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - summ two numbers
+ * @x: first num
+ * @y: secind num
+ * Return: return sum of two
+ */
+int add(int x, int y)
+{
+int sum;
+sum = x + y;
+return (sum);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 int _putchar(char c);
 int print_last_digit(int num);
 void jack_bauer(void);
+int add(int, int);
