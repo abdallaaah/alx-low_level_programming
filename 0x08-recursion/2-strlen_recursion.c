9f9521c4d70557bd1d75955e395a2fa30cfb353b
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-04-06 19:58:51 -0700

    done

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen_recursion - kdnkf
+ * @s: skmdss
+ * Reutrn: number
+ */
+int _strlen_recursion(char *s)
+{
+int len;
+len = strlen(s);
+return len;
+}
