71ad1c7b6149dd23154bf450a87331eaa0886dab
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-04-09 10:43:54 -0700

    don e

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,21 @@
 #include "main.h"
 #include <stdio.h>
 #include <math.h>
+/**
+ * _sqrt_recursion - check square version
+ * @n: number we wil find
+ * Return: int nuber
+ */
 int _sqrt_recursion(int n)
 {
-return (_srq(n,1));
+return (_srq(n, 1));
 }
-
+/**
+ * _srq - complete the first funciotn with rec
+ * @n: the number we pass
+ * @i: the numbers we try
+ * Return: return the number
+ */
 int _srq(int n, int i)
 {
 int sqrnum;
@@ -22,7 +32,7 @@ return (i);
 }
 else
 {
-return (_srq(n,i + 1));
+return (_srq(n, i + 1));
 }
 }
 }
