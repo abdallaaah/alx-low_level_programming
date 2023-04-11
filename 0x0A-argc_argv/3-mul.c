9b6fcf2883afb5d387f3692614f9f26c7d84c0fd
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-04-10 19:18:18 -0700

    type 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multible two arg
+ * @argc: argc is the number of arguments
+ * @argv: the string we need to use
+ * Return: return the string
+ */
+int main(int argc, char *argv[])
+{
+int r;
+(void)argc;
+if (argc < 2)
+{
+printf("Error\n");
+return (1);
+}
+r = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", r);
+return (0);
+}
