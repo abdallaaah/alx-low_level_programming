4ca8ccaec206471f8069a8a6d5877dad8373943c
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-04-10 18:25:20 -0700

    task two

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - return the count of argc
+ * @argc: the number count
+ * @argv: the string
+ * Return: the number of argc
+ */
+int main( int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
