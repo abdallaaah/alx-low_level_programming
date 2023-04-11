2200105aa0aebbf6285ab1f1a393c009b4742d90
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-04-10 19:27:13 -0700

    done

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,13 +9,15 @@
 int main(int argc, char *argv[])
 {
 int r;
-(void)argc;
 if (argc == 3)
 {
-printf("Error\n");
+r = atoi(argv[1]) * atoi(argv[2]);
+printf("%d",r);
+}
+else
+{
+printf ("Error\n");
 return (1);
 }
-r = atoi(argv[1]) * atoi(argv[2]);
-printf("%d\n", r);
 return (0);
 }
