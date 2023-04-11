412cd549cf60f085d5a595a1e2c5633cecbbced5
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-04-10 19:50:34 -0700

    done

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+int i;
+int result;
+if (argc < 1)
+{
+printf("Error\n");
+}
+for (i = 1; i < argc; i++)
+{
+if (atoi(argv[i]))
+{
+result += atoi(argv[i]);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}
+printf("%d\n", result);
+return (0);
+}
