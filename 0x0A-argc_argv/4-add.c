5efcd90c056eaaa4cf543de188bca674fcb0ed97
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-04-10 19:53:20 -0700

    dg

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main: the main function
+ * @argc: argc count
+ * @argv: the string
+ * Return: number plus
+ */
 int main(int argc, char *argv[])
 {
 int i;
 int result;
-if (argc < 1)
+if (argc <= 1)
 {
 printf("Error\n");
 }
+else
+{
 for (i = 1; i < argc; i++)
 {
 if (atoi(argv[i]))
@@ -21,5 +29,6 @@ return (1);
 }
 }
 printf("%d\n", result);
+}
 return (0);
 }
