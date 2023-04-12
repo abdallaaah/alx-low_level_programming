5402731b1ddeedccfdb887141576bde42254a6d3
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-04-11 20:12:41 -0700

    che

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,21 +11,14 @@ char *create_array(unsigned int size, char c)
 {
 char *str;
 unsigned int i;
-if (size == 0)
+str = malloc(sizeof(char) * size);
+if (str == NULL || size == 0)
 {
 return (NULL);
 }
-else if (size > 0)
-{
-str = malloc(sizeof(char) * size);
 for (i = 0; i < size; i++)
 {
 str[i] = c;
 }
 return (str);
-}
-else
-{
-return (NULL);
-}
 }
