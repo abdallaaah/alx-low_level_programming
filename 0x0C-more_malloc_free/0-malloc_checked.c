e27e5771c8072520fc0ee1535f24539a9bfd731d
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-04-13 17:41:41 -0700

    checl new

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,13 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * malloc_checked - reserve a memory in heap
+ * @b: the amount of memory
+ * Return: void
+ */
 void *malloc_checked(unsigned int b)
 {
 void *ptr;
 ptr = malloc(b);
 if (ptr == NULL)
 {
-exit (98);
+exit(98);
 }
 return (ptr);
 }
