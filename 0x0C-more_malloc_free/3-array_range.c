4e9651b411fac8fdc8a5bb20279085a1e6874057
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-04-13 19:22:22 -0700

    doen

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,15 +11,22 @@ int *array_range(int min, int max)
 {
 int size;
 int *ptr;
+int i;
+i = 0;
 if (min > max)
 {
 return (NULL);
 }
-size = max - min;
+size = max - min + 1;
 ptr = malloc(sizeof(int) * size);
 if (ptr == NULL)
 {
 return (NULL);
+}
+for (i = 0; i < size; i++)
+{
+ptr[i] = min;
+min++;
 }
 return (ptr);
 }
