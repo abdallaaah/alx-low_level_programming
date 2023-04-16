ec73bc0ce077f1dcf3ddac372a8a0dad72c19e61
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-04-16 07:40:11 -0700

    tru

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,40 +3,29 @@
 #include <stdlib.h>
 #include <time.h>
   
-// Function to randomly generates password
-// of length N
+
 char* randomPasswordGeneration(int N, char* password)
 {
-    // Initialize counter
+   
     int i = 0;
   
     int randomizer = 0;
   
-    // Seed the random-number generator
-    // with current time so that the
-    // numbers will be different every time
     srand((unsigned int)(time(NULL)));
-  
-    // Array of numbers
     char numbers[] = "0123456789";
   
-    // Array of small alphabets
+   
     char letter[] = "abcdefghijklmnoqprstuvwyzx";
   
-    // Array of capital alphabets
+   
     char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
   
-    // Array of all the special symbols
+    
     char symbols[] = "!@#$^&*?";
-  
-    // Stores the random password
-
-  
-    // To select the randomizer
-    // inside the loop
+ 
     randomizer = rand() % 4;
   
-    // Iterate over the range [0, N]
+ 
     for (i = 0; i < N; i++) {
   
         if (randomizer == 1) {
@@ -58,16 +47,10 @@ char* randomPasswordGeneration(int N, char* password)
     }
    return password;
 }
-  
-// Driver Code
 int main()
 {
-    // Length of the password to
-    // be generated
     int N = 10;
     char* pass;
-  
-    // Function Call
    pass = malloc(sizeof(char) * N);
    pass = randomPasswordGeneration(N, pass);
    printf("%s\n", pass);
