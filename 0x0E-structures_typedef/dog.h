411c6bc635936e83f6020e2c4a00c896a7db2f68
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-04-17 17:59:05 -0700

    checl

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog informations
+ * @name: dog name
+ * @owner: dog owner
+ * @age: age of the dog
+ */
+struct dog
+{
+char *name;
+char *owner;
+float age;
+};
+#endif
