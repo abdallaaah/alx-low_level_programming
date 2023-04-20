8423ac0b3aa6d5835db3c413a6a509aca046b76f
abdallaaah <eng.abdallah.eltawil@gmail.com>
2023-04-19 19:08:49 -0700

    drrr

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,11 +12,11 @@ int i;
 if (array == NULL || size <= 0 || cmp == NULL)
 {
 return (-1);
+}
 for (i = 0; i < size; i++)
 {
-if (cmp(array[i]));
+if (cmp(array[i]))
 return (i);
 }
 return (-1);
-}
 }
