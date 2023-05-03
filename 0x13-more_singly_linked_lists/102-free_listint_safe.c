81505ad5072ec3ce13b0c9facf0aad6633491c2e
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-05-02 19:01:33 -0700

    fix betty

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -9,34 +9,32 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-    size_t len = 0;
-    int diff;
-    listint_t *temp;
+size_t len = 0;
+int diff;
+listint_t *temp;
 
-    if (h == NULL || *h == NULL)
-        return (len);
+if (h == NULL || *h == NULL)
+return (len);
 
-    while (*h)
-    {
-        diff = *h - (*h)->next;
-        if (diff > 0)
-        {
-            temp = (*h)->next;
-            free(*h);
-            *h = temp;
-            len++;
-        }
-        else
-        {
-            free(*h);
-            *h = NULL;
-            len++;
-            break;
-        }
-    }
-
-    *h = NULL;
-
-    return (len);
+while (*h)
+{
+diff = *h - (*h)->next;
+if (diff > 0)
+{
+temp = (*h)->next;
+free(*h);
+*h = temp;
+len++;
+}
+else
+{
+free(*h);
+*h = NULL;
+len++;
+break;
+}
+}
+*h = NULL;
+return (len);
 }
 
