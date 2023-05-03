e41e2787a1a596847b811ba6aa9145a6450a8493
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-05-02 18:05:36 -0700

    task done free list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+void free_listint2(listint_t **head)
+{
+listint_t *ptr;
+if (head == NULL)
+{
+return;
+}
+while (*head)
+{
+ptr = (*head)->next;
+free(*head);
+*head = ptr;
+}
+*head = NULL;
+}
