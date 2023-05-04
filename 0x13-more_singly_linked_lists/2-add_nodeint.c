ad2c9fb0e35df0dcbd03669571bf08e0164c874f
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-05-03 19:28:20 -0700

    check this

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,18 +1,16 @@
 #include "lists.h"
 /**
- * add_nodeint - add node to the start
- * @head: head of list
- * @n: n of func
- * Return: pointer
+ * add_nodeint - add nore to the beganning of linked list
+ * @head: head of linked list
+ * @n: integar value of node
+ * Return: ptr to the node
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 listint_t *ptr;
 ptr = malloc(sizeof(listint_t));
 if (ptr == NULL)
-{
 return (NULL);
-}
 ptr->n = n;
 ptr->next = *head;
 *head = ptr;
