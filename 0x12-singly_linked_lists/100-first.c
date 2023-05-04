52e256b9f52de046d684115bde5371c139d03927
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-05-03 18:25:45 -0700

    new in advane

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * first - function to print specific line before main executed
+ * Return: void
+ */
+void __attribute__((constructor)) first(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n")
+}
