f8a45fe62aff8c496017b08bce73bd6b7d23d225
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-05-24 18:28:23 -0700

    d

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,11 +1,18 @@
 #include "main.h"
-unsigned int flip_bits(unsigned long int n, unsigned long int m) {
-    unsigned long int diff = n ^ m;
-    unsigned int count = 0;
-    while (diff != 0) {
-        count += diff & 1;
-        diff >>= 1; 
-    }
-    return count;
+/**
+ * flip_bits - good function
+ * @n: rhe long int
+ * @m: the long m
+ * Return: count
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int diff = n ^ m;
+unsigned int count = 0;
+while (diff != 0)
+{
+count += diff & 1;
+diff >>= 1;
 }
-
+return (count);
+}
