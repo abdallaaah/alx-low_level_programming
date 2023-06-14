2d76f56f07215df2ee079cee3ab45de7b99461bf
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-06-13 18:02:22 -0700

    checl

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+void free_grid(int **grid, int height)
+{
+int i = 0;
+for (i = 0; i < height; i++)
+{
+free(grid[i]);
+}
+free(grid);
+}
