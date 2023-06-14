01814dccea4eaafc83148348fa9eafb24b5282fc
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-06-14 13:35:45 -0700

    c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - mult two numbers
+ * @argc: count of argv
+ * @argv: the array
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+int res = 0;
+int num1 = 0, num2 = 0;
+if (argc < 3 )
+{
+printf("ERROR\n");
+return (0);
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+res = num1 * num2;
+printf("%d\n", res);
+return (0);
+}
