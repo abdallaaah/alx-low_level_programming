997f89a52e378ef2f9576b27e3516b2a10fc1207
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-06-15 12:56:11 -0700

    seen this

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * array_range - function fill min and max
+ * @min: the least num
+ * @max: the max num
+ * Return: int ptr
+ */
+int *array_range(int min, int max)
+{
+int *ptr;
+int i = 0;
+int s = min;
+int *ptrr;
+if (min > max)
+{
+return (NULL);
+}
+for (i = 0; i <= max; i++)
+{
+i++;
+}
+ptr = malloc(sizeof(int) * i);
+ptrr = ptr;
+if (ptr == NULL)
+{
+free(ptr);
+return (NULL);
+}
+for (i = 0; i <= max; i++)
+{
+*ptr = s;
+ptr++;
+s++;
+}
+return (ptrr);
+}
