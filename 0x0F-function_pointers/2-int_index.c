7ddd383461e5c37de5992c0c3d74095b1c1b6fe5
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-06-22 12:31:47 -0700

    check this

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+/**
+ * int_index - function search on specific number
+ * @array: the array we will search on
+ * @size: size of the array
+ * @cmp: function to the compare
+ * Return - the postion of the array
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i = 0;
+int c = 0;
+if (size <= 0)
+{
+return (-1);
+}
+for(i = 0; i < size; i++)
+{
+c = cmp(array[i]);
+if (c != 0)
+{
+break;
+}
+}
+if (c == 0)
+{
+return (-1);
+}
+return (i);
+}
