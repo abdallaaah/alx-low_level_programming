64404553ce2ef9e080300a0463d343b90b4bc193
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-06-22 11:40:50 -0700

    check task 1

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - function a call back function
+ * @name: name we will pass
+ * @f: pointer to function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
