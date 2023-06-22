e81cc8df9b1a18c0138b67695168300e43d667e3
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-06-22 11:50:11 -0700

    checkk

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,9 @@
 #include "function_pointers.h"
 /**
- * print_name - function a call back function
- * @name: name we will pass
- * @f: pointer to function
- * Return: void
+ * print_name - function to print name
+ * @name: name of human
+ * @f: function pointe
+ * Return - void no return
  */
 void print_name(char *name, void (*f)(char *))
 {
