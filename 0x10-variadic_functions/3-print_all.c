1e98add6cb9a9b5b34e7cb104b44a3ffc8a2d851
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-06-23 16:49:37 -0700

    done inshallah

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,8 +6,7 @@
  */
 void print_all(const char * const format, ...)
 {
-int i = 0;
-int num = 0;
+int i = 0, num = 0;
 double numm = 0;
 char x;
 char *xx;
@@ -17,7 +16,7 @@ while (format && format[i])
 {
 while (format[i])
 {
-switch(format[i])
+switch (format[i])
 {
 case 'c':
 x = va_arg(arg, int);
@@ -34,13 +33,12 @@ break;
 case 's':
 xx = va_arg(arg, char *);
 if (xx == NULL)
-{
 xx = "(nil)";
-}
 printf("%s", xx);
 break;
 }
-if (format[i + 1] != '\0' && (format[i] == 's' || format[i] == 'i' || format[i] == 'c' || format[i] == 'f'))
+if (format[i + 1] != '\0' && (format[i] == 's' || format[i] == 'i'
+|| format[i] == 'c' || format[i] == 'f'))
 printf(", ");
 i++;
 }
