7d3948814a024e98d7d9e79ea341f2052e8df579
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-07-01 17:12:32 -0700

    f

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,6 @@ size_t print_list(const list_t *h)
 size_t num = 0;
 while (h != NULL)
 {
-num++;
 if (h->str == NULL)
 {
 printf("[0] (nil)\n");
@@ -18,6 +17,7 @@ else
 {
 printf("[%d] %s\n", h->len, h->str);
 }
+num++;
 h = h->next;
 }
 return (num);
