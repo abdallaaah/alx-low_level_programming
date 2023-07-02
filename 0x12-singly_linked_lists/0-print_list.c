357d9229cf2eea9557f17f2552462ca5f05009e0
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-07-01 17:07:57 -0700

    task one

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_list - print the linked list itemas
+ * @h: the head of linked list
+ * Return: num of nodes
+ */
+size_t print_list(const list_t *h)
+{
+size_t num = 0;
+while (h != NULL)
+{
+num++;
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%d] %s\n", h->len, h->str);
+}
+h = h->next;
+}
+return (num);
+}
