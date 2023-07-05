773638bed0a72323c6c34778e37fe366eea5bb93
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-07-04 18:55:53 -0700

    check function

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - function to add node in spe index
+ * @head: head of the linked list
+ * @idx: the position
+ * @n: the value
+ * Return: the new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+unsigned int num = 0;
+unsigned int i = 0;
+listint_t *new;
+listint_t *current = *head;
+listint_t *temp = NULL;
+while (current != NULL)
+{
+num++;
+current = current->next;
+}
+if (idx > num)
+{
+return (NULL);
+}
+else
+{
+current = *head;
+new = malloc(sizeof(listint_t));
+new->n = n;
+if (new == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < idx - 1; i++)
+{
+current = current->next;
+}
+temp = current;
+temp = temp->next;
+current->next = new;
+new->next = temp;
+}
+return (new);
+}
