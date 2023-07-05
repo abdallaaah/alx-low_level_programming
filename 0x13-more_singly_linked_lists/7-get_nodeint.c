50e96ef244bc7cefc2fac107f936249cc5df0222
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-07-04 18:20:40 -0700

    cehck

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,14 +8,24 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 unsigned int i = 0;
+unsigned int num = 0;
 listint_t *current = head;
+while (current != NULL)
+{
+num++;
+current = current->next;
+}
+if (index > num)
+{
+return (NULL);
+}
+else
+{
+current = head;
 for (i = 0; i < index; i++)
 {
 current = current->next;
 }
-if (current == NULL)
-{
-return (NULL);
 }
 return (current);
 }
