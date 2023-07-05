31387f47cf04564c9ce7e045590bb75353c657fd
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-07-04 18:24:42 -0700

    check the sumtion taska

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,2 +1,17 @@
 #include "lists.h"
-
+/**
+ * sum_listint - sum the integar list
+ * @head: head of the linked list
+ * Return: the sumtion
+ */
+int sum_listint(listint_t *head)
+{
+listint_t *current = head;
+int sum = 0;
+while (current != NULL)
+{
+sum += current->n;
+current = current->next;
+}
+return (sum);
+}
