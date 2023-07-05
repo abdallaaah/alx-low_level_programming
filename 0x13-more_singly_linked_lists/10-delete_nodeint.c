5f75788507dce18ea515a6cae9ecc6b04f1ebd0e
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-07-04 19:30:11 -0700

    check the deleted node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,9 +1,15 @@
 #include "lists.h"
+/**
+ * delete_nodeint_at_index - delet index in specific node
+ * @head: head of linked
+ * @index: index of deleted
+ * Return: 1 on success
+ */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 listint_t *current = *head;
 listint_t *temp = NULL;
-listint_t *temp2= NULL;
+listint_t *temp2 = NULL;
 listint_t *tempzero = NULL;
 unsigned int num = 0;
 unsigned int i = 0;
