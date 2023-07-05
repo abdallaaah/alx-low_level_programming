73ae34d7e3ac55e3c20414a500ac4e7489d0ba0b
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-07-04 17:34:50 -0700

    check 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2 - list integar function
+ * @head: head of linkedlist
+ * Return: void no return
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *current = *head;
+listint_t *temp = NULL;
+while (current != NULL)
+{
+temp = current;
+current = current->next;
+free(temp);
+}
+}
