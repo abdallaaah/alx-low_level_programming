335cd0816eff1ab107c0c40db3758e1777b57291
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-07-15 13:18:46 -0700

    check this

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,26 +1,24 @@
 #include "main.h"
-/**
- * append_text_to_file - append text to file
- * @filename: filname name
- * @text_content: what we appended
- * Return: unt
- */
 int append_text_to_file(const char *filename, char *text_content)
 {
-FILE *file;
-if (filename == NULL)
-return (-1);
-file = fopen(filename, "a");
-if (file == NULL)
-return (-1);
-if (text_content != NULL)
+int fd;
+ssize_t num;
+if (filename == NULL || text_content == NULL)
 {
-if (fputs(text_content, file) == EOF)
-{
-fclose(file);
 return (-1);
 }
+fd = open(filename, O_WRONLY | O_APPEND);
+if (fd == -1)
+{
+close(fd);
+return (-1);
 }
-fclose(file);
+num = write(fd, text_content, strlen(text_content));
+if (num == -1)
+{
+close(fd);
+return (-1);
+}
+close(fd);
 return (1);
 }
