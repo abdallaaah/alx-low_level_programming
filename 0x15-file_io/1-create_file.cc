6d62dcee129941338a1f3372e43c7363b5761979
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-07-15 12:47:13 -0700

    check new one

--- 0x15-file_io/1-create_file.cc
+++ 0x15-file_io/1-create_file.cc
@@ -7,27 +7,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int fd;
-ssize_t num_written;
+size_t len;
+FILE *file;
 if (filename == NULL)
-{
 return (-1);
-}
-fd  = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
-if (fd == -1)
-{
+file = fopen(filename, "w");
+if (file == NULL)
 return (-1);
-}
+ftruncate(fileno(file), 0);
 if (text_content != NULL)
 {
-num_written = write(fd, text_content, strlen(text_content));
-if (num_written == -1)
+len = strlen(text_content);
+if (fwrite(text_content, sizeof(char), len, file) != len)
 {
-close(fd);
+fclose(file);
 return (-1);
 }
 }
+fclose(file);
 chmod(filename, S_IRUSR | S_IWUSR);
-close(fd);
 return (1);
 }
