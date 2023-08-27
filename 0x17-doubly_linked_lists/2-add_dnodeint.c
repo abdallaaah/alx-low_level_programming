084357653d455bb5827c421ba966831a4c608271
abdalla <eng.abdallaeltawil@gmail.com>
2023-08-27 15:00:08 -0700

    check task 3

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * add_dnodeint - add point in front
+ * @head: head of the linked list
+ * @n: the data
+ * Return: the new node on begginning
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *new;
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+{
+return (NULL);
+}
+if (head != NULL)
+{
+new->n = n;
+new->next = *head;
+new->prev = NULL;
+*head = new;
+}
+else
+{
+new->n = n;
+new->next = NULL;
+new->prev = NULL;
+*head = new;
+}
+return (head);
+}
