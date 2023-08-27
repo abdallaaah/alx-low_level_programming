f7aa129051084973a6f55635f7630e6dbfe88214
abdalla <eng.abdallaeltawil@gmail.com>
2023-08-27 16:16:13 -0700

    check free task

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - free the list of in
+ * @head: heasd of the dlinked list
+ * Return: void no return
+ */
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *temp = head;
+while (head->next != NULL)
+{
+temp = head;
+head = head->next;
+free(temp);
+}
+free(head);
+}
