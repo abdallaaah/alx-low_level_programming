138dbcdc2a35605ed7a70a70943e85a35822aca2
abdalla <eng.abdallaeltawil@gmail.com>
2023-09-08 16:49:30 -0700

    cechek

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,14 +2,14 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 unsigned long int i = 0;
-hash_table_t *table;
+hash_table_t *table = NULL;
 table = malloc(sizeof(hash_table_t));
 if (table == NULL)
 {
 return (NULL);
 }
 table->size = size;
-table->array = calloc(table->size, sizeof(hash_node_t));
+table->array = malloc(sizeof(hash_node_t *) * size);
 if (table->array == NULL)
 {
 free(table);
