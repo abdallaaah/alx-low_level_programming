0bcf8627ea0d350696673d895c342ffedd75229a
abdalla <eng.abdallaeltawil@gmail.com>
2023-09-08 16:30:24 -0700

    check

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#define MAX_SIZE 100
+hash_table_t *hash_table_create(unsigned long int size)
+{
+int i = 0;
+hash_table_t *table;
+table = malloc(sizeof(hash_table_t) * 1);
+if (table == NULL)
+{
+return (NULL);
+}
+table->size = size;
+table->array = malloc(sizeof(hash_node_t) * MAX_SIZE);
+for (i = 0; i < MAX_SIZE; i++)
+{
+table->array = NULL;
+}
+return (table);
+}
