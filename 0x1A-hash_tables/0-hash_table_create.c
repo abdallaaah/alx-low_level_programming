91eeac6b17a702a0dffba7b8f2c6acb33b74107f
abdalla <eng.abdallaeltawil@gmail.com>
2023-09-08 16:46:25 -0700

    cechek

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,23 +1,21 @@
 #include "hash_tables.h"
-#include <stdio.h>
-#define MAX_SIZE 100
 hash_table_t *hash_table_create(unsigned long int size)
 {
 unsigned long int i = 0;
 hash_table_t *table;
-table = malloc(sizeof(hash_table_t) * 1);
+table = malloc(sizeof(hash_table_t));
 if (table == NULL)
 {
 return (NULL);
 }
 table->size = size;
-table->array = malloc(sizeof(hash_node_t) * size);
+table->array = calloc(table->size, sizeof(hash_node_t));
 if (table->array == NULL)
 {
 free(table);
 return (NULL);
 }
-for (i = 0; i < size; i++)
+for (i = 0; i < table->size; i++)
 {
 table->array[i] = NULL;
 }
