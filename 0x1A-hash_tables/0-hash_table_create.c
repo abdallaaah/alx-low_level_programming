542518c56f1213acc8b4b1110b9f974c52affe93
abdalla <eng.abdallaeltawil@gmail.com>
2023-09-08 16:51:29 -0700

    cechl hashes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,4 +1,9 @@
 #include "hash_tables.h"
+/**
+ * hash_table_create - function to create table
+ * @size: size of the table any array
+ * Return: new table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 unsigned long int i = 0;
